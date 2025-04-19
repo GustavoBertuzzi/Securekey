7ca60f72b271d044c01e02be59a31c6626a3770d
AndersonMarques0 <119546595+AndersonMarques0@users.noreply.github.com>
2025-04-19 18:10:57 -0300

    configuracoes do tailwind

--- securekey/frontend/src/App.jsx
+++ securekey/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom' ;
 import Login from './pages/Login'
 import Cadastro from './pages/Cadastro'
 import Cofre from './pages/Cofre'
